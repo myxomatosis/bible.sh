cd085e8b4f4ae3b379356b766769f7688dcd517d
myxomatosis <43965797+myxomatosis@users.noreply.github.com>
2018-10-18 08:32:05 -0400

    Update bible.sh
    Complete rewrite to 
    add functions
    Allow for -- or -ver
    bring code to current standard
    leave in verse numbers

--- bible.sh
+++ bible.sh
@@ -1,24 +1,65 @@
 #!/bin/bash
-ver=`echo "\"$@\"" | grep -o "ver=.*" | sed 's/ver=//'`
-#echo "$ver test 1"    ###############TEST##############
-if [ "$ver" == "" ] ; then
-	ver=KJV
+
+# Set user input (iu) variable
+ui=$@
+
+# Set version ($ver) variable
+translation () {
+
+if [ "$(echo $ui | egrep -o 'ver=|\-ver=\-\-ver')" == "" ] ; then
+	export ver=KJV
+	else
+	export ver=$(echo $ui | sed -r 's/.*ver=(.*\>).*/\1/')
 fi
-#echo "$ver test 2"    ###############TEST##############
-passage=`echo "$@" | sed 's/ //g' | sed 's/ver=.*//'`
-bac=`echo "$passage" | cut -d: -f1`
-#echo "$passage test 3"    ###############TEST##############
-if [ "`echo "$passage" | grep -o "-"`" == "-" ] ; then
-#	echo "$bac test 4"    ###############TEST##############
-	ver1=`echo "$passage" | cut -d: -f2 | cut -d\- -f1`
-#	echo "$ver1 test 5"    ###############TEST##############
-        ver2=`echo "$passage" | cut -d: -f2 | cut -d\- -f2`
-#	echo "$ver2 test 6"    ###############TEST##############
-	for i in $(seq $ver1 $ver2) ; do
-#	echo "https://www.biblegateway.com/passage/?search=$bac:$i&version=$ver test 7"    ###############TEST##############
-	curl -s "https://www.biblegateway.com/passage/?search=$bac:$i&version=$ver" | grep '.*passage-display.*">' | html2text | sed -e 's/[0-9]*Â //' -e 's/â/“/g' -e 's/â/”/g' -e 's/â/‘/g' -e 's/â/’/g' -e 's/â/—/g'
-	echo ""
+
+}
+
+passage_bac_varibles () {
+
+# Set passage variable by removing space and version
+export passage=$(echo $ui | sed -r "s/[\-]*ver=.*\>//" | sed 's/ //g')
+
+# Set book and chapter (bac) variable by removing anything before the colon
+export bac=$(echo "$passage" | cut -d: -f1)
+
+}
+
+hyphen () {
+
+	# Set verse 1 (ver1) variable
+	ver1=$(echo "$passage" | cut -d: -f2 | cut -d\- -f1)
+	# Set verse 2 (ver2) variable
+	ver2=`echo "$passage" | cut -d: -f2 | cut -d\- -f2`
+	for num in $(seq $ver1 $ver2) ; do
+		curl -s "https://www.biblegateway.com/passage/?search=$bac:$num&version=$ver" | grep '.*passage-display.*">' | html2text | sed -e 's/[0-9]*Â //' -e 's/â/“/g' -e 's/â/”/g' -e 's/â/‘/g' -e 's/â/’/g' -e 's/â/—/g'
+		echo ""
 	done
-else curl -s "https://www.biblegateway.com/passage/?search=$passage&version=$ver" | grep '.*passage-display.*">' | html2text | sed -e 's/[0-9]*Â //' -e 's/â/“/g' -e 's/â/”/g' -e 's/â/‘/g' -e 's/â/’/g' -e 's/â/—/g'
-#echo "https://www.biblegateway.com/passage/?search=$bac:$i&version=$ver test 8"    ###############TEST##############
+
+}
+
+whole_chapter () {
+
+curl -s "https://www.biblegateway.com/passage/?search=$passage&version=$ver" | grep '.*passage-display.*">' | html2text | sed -e 's/â<80><9d>/“/g' -e 's/â<80><9c>/”/g' -e 's/â<80><98>/‘/g' -e 's/â<80><99>/’/g' -e 's/â<80><94>/—/g' | sed -r '2s/[0-9]+Â/1/' | sed -r 's/([0-9]+)Â/\1/'
+
+}
+
+logic () {
+
+#Check for hyphens
+if [ "$(echo "$passage" | grep -o "-")" == "-" ] ; then
+	# If there is a hyphen run hyphen function
+	hyphen
+else 
+	#Check if there are any verses
+	if ! [ "$(echo "$passage" | grep -o ":")" == ":" ] ; then
+		whole_chapter
+	else
+		curl -s "https://www.biblegateway.com/passage/?search=$passage&version=$ver" | grep '.*passage-display.*">' | html2text | sed -e 's/[0-9]*Â //' -e 's/â/“/g' -e 's/â/”/g' -e 's/â/‘/g' -e 's/â/’/g' -e 's/â/—/g'
+	fi
 fi
+
+}
+
+translation
+passage_bac_varibles
+logic
